395b4438e9504f1d0930521e538d48a52c34d5dd
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2020-10-07 17:17:14 +0800

    Update and rename 7001_theCar.py to 7002_theCar.py

---
+++ 7002_theCar.py
@@ -0,0 +1,19 @@
+
+class Car:
+  def __init__ (self, brand, colour, speed):
+    self.brand = brand
+    self.colour = colour
+    self.speed = speed
+    
+  def whoAmI(self, brand, colour):
+    print("Hi I am a " + colour + " car")
+    
+  def upgrade(self, speed)
+    speed += 10
+    print("Your speed has increased by " + str(speed))
+    
+  
+    
+  
+    
+
