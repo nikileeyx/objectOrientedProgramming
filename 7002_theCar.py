af3401584416a7804ab67b65024536a4f3eb5b68
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2020-10-07 17:25:15 +0800

    Update 7002_theCar.py

--- 7002_theCar.py
+++ 7002_theCar.py
@@ -1,19 +1,23 @@
-
 class Car:
-  def __init__ (self, brand, colour, speed):
+  def __init__(self, brand, colour, speed):
     self.brand = brand
     self.colour = colour
     self.speed = speed
     
-  def whoAmI(self, brand, colour):
-    print("Hi I am a " + colour + " car")
+  def whoAmI(self):
+    print("Hi I am a " + self.colour + " car")
     
-  def upgrade(self, speed)
+  def upgrade(self, speed):
     speed += 10
-    print("Your speed has increased by " + str(speed))
-    
-  
-    
-  
+    print("Your speed has increased by " + str(self.speed))
+    return speed
     
 
+
+'''
+Pass in to Python Shell these commands
+
+car = Car("hello", "yellow", "10")
+car.whoAmI()
+
+'''
